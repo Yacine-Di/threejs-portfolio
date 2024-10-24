318556bcde2e911d0c83ec8db8c47d270b62e882
Yacine Diaf <yacinediafwork@gmail.com>
2024-10-24 14:54:40 +0200

    création du repo, section Hero finie

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './sections/Hero';
+import NavBar from './sections/Navbar';
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <NavBar />
+            <Hero />
+        </main>
+    );
+};
+
+export default App;
