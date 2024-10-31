403eeae486ace838e5624d82649ae6c552243b65
Yacine Diaf <yacinediafwork@gmail.com>
2024-10-31 11:58:29 +0100

    traduction text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
                 <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space>">
                     <a href="#about" className="w-fit">
                         <Button
-                            name="Travaillons ensemble"
+                            name="Let's work together"
                             isBeam
                             containerClass="sm:w-fit w-full sm:min-w-96"
                         />
